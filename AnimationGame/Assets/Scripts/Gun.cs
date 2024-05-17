c487cbef6cf8816268029b593f8929f408d18331
jpaviour <jpaviour64@gmail.com>
2024-05-17 16:28:28 +0100

    more timeline

---
+++ AnimationGame/Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 1;
+
+    public void Shoot()
+    {
+
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        // Rotate the bullet 90 degrees on the z-axis
+        bullet.transform.Rotate(90, 0, 0);
+
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.forward * bulletSpeed;
+        
+    }
+}
